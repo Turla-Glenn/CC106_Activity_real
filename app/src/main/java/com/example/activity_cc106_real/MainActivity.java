230261b186d64be50c91e3b0ffaa34c362b94d5e
Glenn Turla <turlaglenn@gmail.com>
2024-03-07 20:06:05 +0800

    all project cc106

---
+++ app/src/main/java/com/example/activity_cc106_real/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.activity_cc106_real;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.tv1);
+        StrictMode.enableDefaults();
+
+        try {
+            URL url = new URL("http://192.168.100.155:8080/myapplication/connect.php");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            InputStream inputStream = connection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder result = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                result.append(line);
+            }
+
+            Toast.makeText(getApplicationContext(), "PHP MYSQL CONNECT...", Toast.LENGTH_LONG).show();
+            Log.e("pass 1", "Connection Success");
+            tv.setText("Connected Successfully....");
+
+            reader.close();
+            inputStream.close();
+        } catch (Exception e) {
+            Log.e("Fail 1", e.toString());
+            Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void goToNextActivity(View view) {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/activity_cc106_real/MainActivity3.java
@@ -0,0 +1,22 @@
+package com.example.activity_cc106_real;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+
+
+    public void goToNextActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
